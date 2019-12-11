96e07c182098abbe9c362898380e89b3a36db9e6
shanedabes <shane@donohoe.cc>
2019-12-11 23:38:29 +0000

    add exit script

--- exit.sh
+++ exit.sh
@@ -3,19 +3,11 @@ lock() {
     lxdm -c USER_SWITCH
 }
 
-pre_exit() {
-    killall mopidy
-    while [ ! -e ~/.local/share/mopidy/core/state.json.gz ] ; do
-        sleep 1
-    done
-}
-
 case "$1" in
     lock)
         light-locker-command -l
         ;;
     logout)
-        pre_exit
         bspc quit
         ;;
     suspend)
@@ -25,11 +17,9 @@ case "$1" in
         systemctl hibernate
         ;;
     reboot)
-        pre_exit
         systemctl reboot
         ;;
     shutdown)
-        pre_exit
         systemctl poweroff
         ;;
     *)
