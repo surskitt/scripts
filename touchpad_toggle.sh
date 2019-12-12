72905534b6013d28ba99cca7b78a1cfad4821d70
shanedabes <shane@donohoe.cc>
2019-12-12 00:53:08 +0000

    add touchpad toggle script

---
+++ touchpad_toggle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+device="Elan Touchpad"
+state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
+
+if [ "${state}" == '1' ];then
+  xinput disable "${device}"
+else
+  xinput enable "${device}"
+fi
