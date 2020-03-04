a530168b2e3da22416334236c7e23342f59856be
shanedabes <shane@donohoe.cc>
2020-03-04 02:22:31 +0000

    reformatting

--- poly_notify.sh
+++ poly_notify.sh
@@ -6,12 +6,9 @@ NOTIFY_LENGTH=3
 cmd="${1}"
 args="${@:2}"
 
-mr=$(grep modules-right "${POLYBAR_CONFIG}")
-
 echo "${args}" >> /tmp/polybar_notify
 
+mr=$(grep modules-right "${POLYBAR_CONFIG}")
 sed -i "s/modules-right.*/modules-right = notify/" "${POLYBAR_CONFIG}"
-
 sleep "${NOTIFY_LENGTH}"
-
 sed -i "s/modules-right.*/${mr}/" "${POLYBAR_CONFIG}"
