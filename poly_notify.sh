821eb13511130fe9946cc643bbac7a3b7e8451ae
shanedabes <shane@donohoe.cc>
2020-03-04 03:12:34 +0000

    allow multiple executions

--- poly_notify.sh
+++ poly_notify.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
 POLYBAR_CONFIG="${HOME}/.config/polybar/config"
+NOTIFY_QUEUE=/tmp/polybar_notify
 NOTIFY_LENGTH=3
 
 cmd="${1}"
 args="${@:2}"
 
-echo "${args}" >> /tmp/polybar_notify
+echo "${args}" >> "${NOTIFY_QUEUE}"
+initial_queue_stat="$(stat -c '%Y' ${NOTIFY_QUEUE})"
 
-mr=$(grep modules-right "${POLYBAR_CONFIG}")
-sed -i "s/modules-right.*/modules-right = notify/" "${POLYBAR_CONFIG}"
+if [[ ! -f /tmp/polybar_config_bak ]]; then
+    cp "${POLYBAR_CONFIG}" /tmp/polybar_config_bak
+fi
+
+if ! grep -q "modules-right = notify" "${POLYBAR_CONFIG}"; then
+    sed -i "s/modules-right.*/modules-right = notify/" "${POLYBAR_CONFIG}"
+fi
+
 sleep "${NOTIFY_LENGTH}"
-sed -i "s/modules-right.*/${mr}/" "${POLYBAR_CONFIG}"
+
+after_queue_stat="$(stat -c '%Y' ${NOTIFY_QUEUE})"
+
+if [[ "${initial_queue_stat}" = "${after_queue_stat}" ]]; then
+    cp /tmp/polybar_config_bak "${POLYBAR_CONFIG}"
+    rm /tmp/polybar_config_bak
+fi
