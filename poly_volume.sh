c531f8ae26a53c77677f4d4002e7b7e93894074e
shanedabes <shane@donohoe.cc>
2020-03-04 03:27:55 +0000

    add script for controlling volume with polybar notifications

---
+++ poly_volume.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+VOLUME_STEP=5
+
+get_icon() {
+    case "${1}" in
+      mute)
+        echo ""
+        ;;
+      0)
+        echo ""
+        ;;
+      [1-4][0-9]|[0-9])
+        echo ""
+        ;;
+      *)
+        echo ""
+        ;;
+    esac
+}
+
+notify() {
+    script_dir="$(dirname $0)"
+
+    if pamixer --get-mute >/dev/null ; then
+        "${script_dir}/poly_notify.sh" "$(get_icon mute) Volume muted"
+    else
+        volume="$(pamixer --get-volume)"
+        icon=$(get_icon "${volume}")
+        "${script_dir}/poly_notify.sh" "${icon} Volume ${volume}%"
+    fi
+}
+
+case "${1}" in
+  up)
+    pamixer -i "${VOLUME_STEP}"
+    ;;
+  down)
+    pamixer -d "${VOLUME_STEP}"
+    ;;
+  mute)
+    pamixer -t
+    ;;
+esac
+
+notify
