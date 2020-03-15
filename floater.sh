f70fc767c2e4a8336b02fca9b1a5f50a25c905c9
shanedabes <shane@donohoe.cc>
2020-03-15 20:04:54 +0000

    simplify floater script

---
+++ floater.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo "Usage: ${0} [-b]"
+}
+
+while getopts 'bh' opt; do
+    case "${opt}" in
+        b)
+            FOCUS_ARG="focus=off"
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        ?)
+            usage >&2
+            exit 1
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+bspc rule -a \* -o state=floating "${FOCUS_ARG}"
+${*}
