6eeff88269069b4f6961ef93c3e0385d378d6a74
shanedabes <shane@donohoe.cc>
2020-03-17 02:27:19 +0000

    add script to set wallpaper

---
+++ wp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+files=(${@})
+filecount="${#files[@]}"
+
+monitors="$(xrandr --listactivemonitors|head -1|cut -d ' ' -f 2)"
+
+for f in "${files[@]:0:${monitors}}"; do
+    if [ ! -f "${f}" ]; then
+        echo "ERROR: ${f} does not exist" >&2
+        exit 1
+    fi
+done
+
+feh --bg-fill ${files[@]:0:${monitors}}
+
+for i in $(seq ${monitors}); do
+    n=$((i-1))
+
+    file="${files[${n}]}"
+    if [[ ${file} != "" ]]; then
+        ln -sf "${file}" ~/.bg${i}
+    fi
+done
