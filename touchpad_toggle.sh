0e6f65877baeb2b4fa9726bb52d4bb6b02ed1d55
shanedabes <shane@donohoe.cc>
2020-03-26 03:03:01 +0000

    set touchpadd toggle to work on teal

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device="Elan Touchpad"
+device="CUST0001:00 06CB:76AF Touchpad"
 state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
 
 if [ "${state}" == '1' ];then
