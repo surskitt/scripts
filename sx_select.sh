2b39222fd7d17f1411bc47cfcaa0d2edc39318b7
shanedabes <shane@isda.best>
2020-08-11 02:05:47 +0100

    add caching to sx selector script

--- sx_select.sh
+++ sx_select.sh
@@ -8,13 +8,33 @@ else
     dir="${1}"
 fi
 
-f=$(ls -1 -d "${dir}"/*/ 2>/dev/null ; find "${dir}" -maxdepth 1 -type f -not -path '*/\.*')
+if ! [ -d "${dir}" ]; then
+    echo "error: ${dir} is not a directory" >&2
+    exit 1
+fi
 
-fl=$(echo "${f}"|while read fn; do
-    p=$(previewer.sh "${fn}")
+no_refresh=true
 
-    echo "${p}${TAB}${fn}"
-done)
+cache_size=$(wc -l "${dir}/.thumbs.tsv" 2>/dev/null|cut -d ' ' -f 1 || 0)
+file_count="$(ls "${dir}"|wc -l)"
+
+if [[ ${cache_size} -ne ${file_count} ]]; then
+    no_refresh=false
+fi
+
+if [[ -f "${dir}/.thumbs.tsv" && $no_refresh == true ]]; then
+    fl=$(< "${dir}/.thumbs.tsv")
+else
+    f=$(ls -1 -d "${dir}"/*/ 2>/dev/null ; find "${dir}" -maxdepth 1 -type f -not -path '*/\.*')
+
+    fl=$(echo "${f}"|while read fn; do
+        p=$(previewer.sh "${fn}")
+
+        echo "${p}${TAB}${fn}"
+    done)
+
+    echo "${fl}" > "${dir}/.thumbs.tsv"
+fi
 
 s=$(echo "${fl}"|while IFS=$"${TAB}" read fn _; do echo "${fn}"; done|sxiv -ftio|head -1)
 
