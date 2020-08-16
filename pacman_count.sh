e889fcf581e014194fc295dbf6ad13b9227c695e
shanedabes <shane@isda.best>
2020-08-16 15:13:50 +0100

    add quiet option

--- pacman_count.sh
+++ pacman_count.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+usage() {
+    echo "Usage: ${0} [-q]"
+}
+
+MSG=" new packages"
+
+while getopts 'qh' opt; do
+    case "${opt}" in
+        q)
+            MSG=""
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        ?)
+            usage >&2
+            exit 1
+    esac
+done
+shift $(( OPTIND - 1 ))
+
 count="$(pacman -Supq|wc -l)"
 
-echo " ${count} new packages"
+echo " ${count}${MSG}"
