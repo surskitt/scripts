c9c4c1a7247d3cfec41923223c28736d01d8c77f
shanedabes <shane@isda.best>
2020-08-22 02:36:00 +0100

    add more fields to notification

--- wttr.sh
+++ wttr.sh
@@ -65,88 +65,104 @@ get_icon() {
        *Light\ rain)
            echo 
            ;;
-       # Moderate\ rain\ at\ times)
-       #     echo
-       #     ;;
-       # Moderate\ rain)
-       #     echo
-       #     ;;
-       # Heavy\ rain\ at\ times)
-       #     echo
-       #     ;;
-       # Heavy\ rain)
-       #     echo
-       #     ;;
-       # Light\ freezing\ rain)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ freezing\ rain)
-       #     echo
-       #     ;;
-       # Light\ sleet)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ sleet)
-       #     echo
-       #     ;;
-       # Patchy\ light\ snow)
-       #     echo
-       #     ;;
-       # Light\ snow)
-       #     echo
-       #     ;;
-       # Patchy\ moderate\ snow)
-       #     echo
-       #     ;;
-       # Moderate\ snow)
-       #     echo
-       #     ;;
-       # Patchy\ heavy\ snow)
-       #     echo
-       #     ;;
-       # Heavy\ snow)
-       #     echo
-       #     ;;
-       # Ice\ pellets)
-       #     echo
-       #     ;;
-       # Light\ rain\ shower)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ rain\ shower)
-       #     echo
-       #     ;;
-       # Torrential\ rain\ shower)
-       #     echo
-       #     ;;
-       # Light\ sleet\ showers)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ sleet\ showers)
-       #     echo
-       #     ;;
-       # Light\ snow\ showers)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ snow\ showers)
-       #     echo
-       #     ;;
-       # Patchy\ light\ rain\ with\ thunder)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ rain\ with\ thunder)
-       #     echo
-       #     ;;
-       # Patchy\ light\ snow\ with\ thunder)
-       #     echo
-       #     ;;
-       # Moderate\ or\ heavy\ snow\ with\ thunder)
-       #     echo
-       #     ;;
+       Moderate\ rain\ at\ times)
+           echo 
+           ;;
+       Moderate\ rain)
+           echo 
+           ;;
+       Heavy\ rain\ at\ times)
+           echo 
+           ;;
+       Heavy\ rain)
+           echo 
+           ;;
+       Light\ freezing\ rain)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ freezing\ rain)
+           echo 
+           ;;
+       Light\ sleet)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ sleet)
+           echo 
+           ;;
+       Patchy\ light\ snow)
+           echo 流
+           ;;
+       Light\ snow)
+           echo 流
+           ;;
+       Patchy\ moderate\ snow)
+           echo 流
+           ;;
+       Moderate\ snow)
+           echo 流
+           ;;
+       Patchy\ heavy\ snow)
+           echo 
+           ;;
+       Heavy\ snow)
+           echo 
+           ;;
+       Ice\ pellets)
+           echo 
+           ;;
+       Light\ rain\ shower)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ rain\ shower)
+           echo 
+           ;;
+       Torrential\ rain\ shower)
+           echo 
+           ;;
+       Light\ sleet\ showers)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ sleet\ showers)
+           echo 
+           ;;
+       Light\ snow\ showers)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ snow\ showers)
+           echo 
+           ;;
+       Patchy\ light\ rain\ with\ thunder)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ rain\ with\ thunder)
+           echo 
+           ;;
+       Patchy\ light\ snow\ with\ thunder)
+           echo 
+           ;;
+       Moderate\ or\ heavy\ snow\ with\ thunder)
+           echo 
+           ;;
         *)
            echo "?"
            ;;
    esac
+}
+
+notify_body() {
+    humidity="${1}"
+    wind="${2}"
+    sunrise="${3}"
+    dawn="${4}"
+    sunset="${5}"
+    dusk="${6}"
+
+    l1="💧 ${humidity/\\/}"
+    l2="🌬️ ${wind}"
+    l3="🌅 ${sunrise%:*} (${dawn%:*})"
+    l4="🌇 ${sunset%:*} (${dusk%:*})"
+
+    printf "\n%s\n%s\n%s\n%s\n" "${l1}" "${l2}" "${l3}" "${l4}"
 }
 
 NOTIFY=false
@@ -163,14 +179,17 @@ while getopts "n" opt; do
     esac
 done
 
-w=$(curl -s wttr.in/?format='%c+%C__%t\n')
+w=$(curl -s wttr.in/?format='%c_%C_%t_%h_%w_%S_%D_%s_%d\n')
 
-cond="${w%%__*}"
-status="${w##*__}"
+IFS="_" read -r emoji cond temp humidity wind sunrise dawn sunset dusk <<< "${w}"
+
 icon=$(get_icon "$cond")
 
 if $NOTIFY; then
-    notify-send "${cond}" "${status/+/}"
+    title="${emoji} ${cond} ${temp#+}"
+    body=$(notify_body "${humidity}" "${wind}" "${sunrise}" "${dawn}" "${sunset}" "${dusk}")
+
+    notify-send "${title}" "${body}"
 else
-    echo "${icon} ${status/+/}"
+    echo "${icon} ${temp/+/}"
 fi
